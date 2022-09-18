569d486595eea6110963597023b5531560ba2e89
Aníbal <maosistemasrosario@gmail.com>
2022-09-18 20:16:32 -0300

    strings

---
+++ string.py
@@ -0,0 +1,15 @@
+mensaje = 'hola mundo'
+print(mensaje.capitalize())
+
+msg1 = 'hola '
+msg2 = 'mundo'
+print(msg1.capitalize()+msg2.capitalize())
+
+print(type(mensaje))
+
+nombre = input('Ingrese su nombre: ')
+print(f'Bienvenido {nombre}')
+
+numero = float(input('Ingrese un numero: '))
+print(f'El número ingresado es {numero}')
+print(type(numero))
